1447bc0729a6cb8b75be006a8d36518997c10ccd
unknown <milja.haiko@gmail.com>
2022-09-13 14:00:34 +0300

    third commit

--- src/main/java/s22/bookstore/web/BookController.java
+++ src/main/java/s22/bookstore/web/BookController.java
@@ -1,30 +1,43 @@
 package s22.bookstore.web;
 
-import java.util.ArrayList;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import s22.bookstore.domain.Book;
+import s22.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 	
-	private ArrayList<Book> books = new ArrayList<>();
+	@Autowired
+	private BookRepository repository;
 	
-	@GetMapping("/book")
-	public String showBooks(Model model) {
-		model.addAttribute("booksList", "books");
+	@RequestMapping(value = {"/", "/booklist"})
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+	
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
-		return "books";		
+		return "addbook";
 	}
 	
-	@PostMapping("/book")
-	public String addBook(Book kirja) {
-		books.add(kirja);
-		return "redirect:/book";
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
 	}
-
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	
 }
